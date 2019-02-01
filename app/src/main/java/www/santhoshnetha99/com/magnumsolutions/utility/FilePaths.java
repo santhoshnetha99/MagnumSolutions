1707e075fe1ba5f507cf86cca69f9fbc85b02a10
welcome <santhoshnetha99@gmail.com>
2019-02-01 22:28:12 +0530

    project includes email authentication and verification. it includes a chatroom system and also user profile management

---
+++ app/src/main/java/www/santhoshnetha99/com/magnumsolutions/utility/FilePaths.java
@@ -0,0 +1,11 @@
+package www.santhoshnetha99.com.magnumsolutions.utility;
+
+
+
+public class FilePaths {
+
+    public FilePaths() {
+    }
+
+    public String FIREBASE_IMAGE_STORAGE = "images/users";
+}
